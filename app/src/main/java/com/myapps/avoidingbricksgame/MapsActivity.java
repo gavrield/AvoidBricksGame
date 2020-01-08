de69ee440be3310e216231d4e961e93c5317b28e
gavrield <gavrield@mail.afeka.ac.il>
2020-01-08 19:23:42 +0200

    modifying project

--- app/src/main/java/com/myapps/avoidingbricksgame/MapsActivity.java
+++ app/src/main/java/com/myapps/avoidingbricksgame/MapsActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,6 +21,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private GoogleMap mMap;
     private TableLayout table;
     private TableRow[] tableRows;
+    private TextView[] names;
+    private TextView[] scores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +33,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         table = (TableLayout)findViewById(R.id.table);
-        tableRows = new TableRow[ROWS];
+        createArrays();
         for (int i = 0; i < ROWS; i++){
             table.addView(tableRows[i]);
         }
@@ -54,5 +57,43 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private void createArrays(){
+        tableRows = new TableRow[ROWS];
+        tableRows[0] = findViewById(R.id.row0);
+        tableRows[1] = findViewById(R.id.row1);
+        tableRows[2] = findViewById(R.id.row2);
+        tableRows[3] = findViewById(R.id.row3);
+        tableRows[4] = findViewById(R.id.row4);
+        tableRows[5] = findViewById(R.id.row5);
+        tableRows[6] = findViewById(R.id.row6);
+        tableRows[7] = findViewById(R.id.row7);
+        tableRows[8] = findViewById(R.id.row8);
+        tableRows[9] = findViewById(R.id.row9);
+
+        names = new TextView[ROWS];
+        names[0] = findViewById(R.id.name0);
+        names[1] = findViewById(R.id.name1);
+        names[2] = findViewById(R.id.name2);
+        names[3] = findViewById(R.id.name3);
+        names[4] = findViewById(R.id.name4);
+        names[5] = findViewById(R.id.name5);
+        names[6] = findViewById(R.id.name6);
+        names[7] = findViewById(R.id.name7);
+        names[8] = findViewById(R.id.name8);
+        names[9] = findViewById(R.id.name9);
+
+        scores = new TextView[ROWS];
+        scores[0] = findViewById(R.id.score0);
+        scores[1] = findViewById(R.id.score1);
+        scores[2] = findViewById(R.id.score2);
+        scores[3] = findViewById(R.id.score3);
+        scores[4] = findViewById(R.id.score4);
+        scores[5] = findViewById(R.id.score5);
+        scores[6] = findViewById(R.id.score6);
+        scores[7] = findViewById(R.id.score7);
+        scores[8] = findViewById(R.id.score8);
+        scores[9] = findViewById(R.id.score9);
     }
 }
